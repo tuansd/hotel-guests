05b85e610617f5d8fe1e3d38cfa8706c281a8ada
Tuan Tran <hometran@gmail.com>
2022-10-23 14:50:01 -0700

    getting start

---
+++ src/main/java/com/vietez/lil/hotelguests/HotelGuestsApplication.java
@@ -0,0 +1,13 @@
+package com.vietez.lil.hotelguests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelGuestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelGuestsApplication.class, args);
+	}
+
+}
